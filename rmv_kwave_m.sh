577a3ddde0a0e7d89f391b1d549e76a1f6150eaf
EduMV <edu.marin.vera@gmail.com>
2024-07-01 20:04:38 -0500

    kwave files

---
+++ rmv_kwave_m.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+#SBATCH --gpus-per-node=1
+#SBATCH --nodes=1
+#SBATCH --partition=thinkstation-p360
+#SBATCH --nodelist=worker10
+#SBATCH --output="log.out"
+
+srun matlab -nosplash -nodesktop -nodisplay -r “kwave_dataset_AC; exit”
