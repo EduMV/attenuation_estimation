b0c2b895585d1a7767e6a365c78b2026ae25d4e6
EduMV <edu.marin.vera@gmail.com>
2024-07-03 22:13:24 -0500

    new sh

---
+++ rmv_kwave_new_m.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --nodelist=worker10
+#SBATCH --output="slurm-%j.out"
+#SBATCH --time=10000000
+#SBATCH --partition=thinkstation-p360
+#SBATCH --gres=gpu:1
+
+srun /usr/local/MATLAB/R2023b/bin/matlab -nosplash -nodesktop -nodisplay -r "kwave_dataset_AC; exit
