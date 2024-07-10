0db9fcb18e35928a7c008d84d6a299881b717f92
EduMV <edu.marin.vera@gmail.com>
2024-07-09 19:41:25 -0500

    newbatch 72-90

--- kwave_dataset_AC_v2.m
+++ kwave_dataset_AC_v2.m
@@ -5,7 +5,7 @@ clear; close all; clc; rng shuffle;
 rng("default")
 
 %% Iter
-for sss = 61:80
+for sss = 72:90
 % This example demonstrates the use of k-Wave for the reconstruction of a
 % two-dimensional photoacoustic wave-field recorded  over a linear array of
 % sensor elements  The sensor data is simulated using kspaceFirstOrder2D
